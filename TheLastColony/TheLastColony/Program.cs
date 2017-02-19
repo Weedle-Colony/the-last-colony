e32b2466cb3b7b4792dff8581daec1bc2e1ca1be
Joshua Landreneau <joshlandreneau@yahoo.com>
2017-02-18 21:46:35 -0600

    COL-2 First push to JIRA

--- TheLastColony/TheLastColony/Program.cs
+++ TheLastColony/TheLastColony/Program.cs
@@ -11,6 +11,7 @@ namespace TheLastColony
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Weedlewhomps!");
+            Console.WriteLine("Hello, hello!");
 
             return;
         }
