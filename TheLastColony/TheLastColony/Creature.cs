2714f7a9ee270b80cd5f42e484f38c43e61e7085
Joshua Landreneau <joshlandreneau@yahoo.com>
2017-02-21 18:41:15 -0600

    COL-2 Added lifecycle for creatures

---
+++ TheLastColony/TheLastColony/Creature.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TheLastColony
+{
+    class Creature
+    {
+        public char Gender { get; private set; }
+        public int BirthYear { get; private set; }
+        public int ReproductionRate { get; private set; }
+        public int Age => Program.CurrentYear - BirthYear;
+        public int MaxAge { get; private set; }
+        //Will not keep DeathYear property; using for testing
+        public int DeathYear { get; set; }
+
+        private static readonly Random _random = new Random();
+        private static List<Creature> _offspringList = new List<Creature>();
+
+        public Creature()
+        {
+            BirthYear = Program.CurrentYear;
+            ComputeMaxAgeAndGender();          
+        }
+
+        private void ComputeMaxAgeAndGender()
+        {
+            int lifeExpectancyBracket = _random.Next(100);
+
+            //50% chance to be either Male or Female
+            Gender = lifeExpectancyBracket % 2 == 0 ? 'M' : 'F';
+
+            //60% chance to live past 50 years.
+            //Can yield 1-5 baby creatures.
+            if (lifeExpectancyBracket > 40)
+            {
+                MaxAge = _random.Next(50, 80);
+                ReproductionRate = _random.Next(15, 49);
+            }
+            else
+            {
+                //30% chance to live between 20 and 50 years.
+                //Can yield 1-3 baby creatures.
+                if (lifeExpectancyBracket > 10 && lifeExpectancyBracket <= 40)
+                {
+                    MaxAge = _random.Next(20, 49);
+                    ReproductionRate = _random.Next(15, 19);
+                }
+
+                //10% chance to live under 20 years.
+                //Can yield up to 1 baby creature.
+                else
+                {
+                    MaxAge = _random.Next(1, 19);
+                    ReproductionRate = _random.Next(15, 19);
+                }
+            }
+        }
+
+        public static List<Creature> ProduceOffspring(List<Creature> creatureList)
+        {
+            _offspringList.Clear();
+            
+            var motherList = creatureList.Where(c => c.Gender == 'F' && (c.Age >= c.ReproductionRate) && c.Age % c.ReproductionRate == 0);
+
+            if(motherList.Count() == 0)
+            {
+                return _offspringList;
+            }
+            else
+            {
+                foreach (var creature in motherList)
+                {
+                    _offspringList.Add(CreateCreature(creature.GetType().Name));
+                }
+                return _offspringList;
+            }
+        }
+
+        private static Creature CreateCreature(string creatureType)
+        {
+            switch (creatureType)
+            {
+                case "Carnivore":
+                    return new Carnivore();
+                case "Herbivore":
+                    return new Herbivore(); 
+                case "Omnivore":
+                    return new Omnivore();              
+            }
+
+            return null;
+        }
+
+        public void Eat()
+        {
+            //Difficult to say how much a creature gains at this point.
+            MaxAge += 2;
+        }
+    }
+
+    class Carnivore : Creature
+    {
+    }
+
+    class Herbivore : Creature
+    {
+    }
+
+    class Omnivore : Creature
+    {
+    }
+}
--- TheLastColony/TheLastColony/Program.cs
+++ TheLastColony/TheLastColony/Program.cs
@@ -1,19 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TheLastColony
 {
     class Program
     {
-        static void Main(string[] args)
+        public static int CurrentYear { get; set; } = 0;
+        static void Main()
         {
-            Console.WriteLine("Hello Weedlewhomps!");
-            Console.WriteLine("Hello, hello!");
+            List<Creature> initialList = new List<Creature>() {
+            new Carnivore(),
+            new Herbivore(),
+            new Omnivore()
+            };
 
-            return;
+            //Will not keep this list; using for testing
+            List<Creature> deadList = new List<Creature>();
+
+            foreach (Creature creature in initialList)
+            {
+                Console.WriteLine($"Original {creature.GetType().Name} with Gender [{creature.Gender}] and MaxAge {creature.MaxAge}");
+            }
+
+
+            while (initialList.Any() && CurrentYear < 1000)
+            {
+                //Death     
+                if (initialList.Any(c => c.Age == c.MaxAge))
+                {
+                    foreach (Creature creature in initialList.Where(c => c.Age == c.MaxAge))
+                    {
+                        creature.DeathYear = CurrentYear;
+                        deadList.Add(creature);
+                    }
+
+                    initialList.RemoveAll(c => c.Age == c.MaxAge);
+                }
+
+                //Reproduction (if there are any fertile females)
+                initialList.AddRange(Creature.ProduceOffspring(initialList));
+
+                //New year
+                CurrentYear++;
+            }
+
+            Console.WriteLine($"\n{deadList.Count()} creatures self-sustained until Year: " + --CurrentYear);
+
+            foreach (var creature in deadList.OrderBy(c => c.BirthYear))
+            {
+                Console.WriteLine($"{creature.GetType().Name} [{creature.Gender}] lived to be {creature.DeathYear - creature.BirthYear}, born in Year {creature.BirthYear} and died in Year {creature.DeathYear}");
+            }
         }
     }
 }
